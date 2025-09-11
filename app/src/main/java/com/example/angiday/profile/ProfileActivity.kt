a01c2bd2551ea3c10b3fcd6216997273f744b1cb
TuanDuy <duongtuanduy1@gmail.com>
2025-09-11 22:19:08 +0700

    profile

--- app/src/main/java/com/example/angiday/profile/ProfileActivity.kt
+++ app/src/main/java/com/example/angiday/profile/ProfileActivity.kt
@@ -1,33 +1,32 @@
-package com.example.angiday.activity
+package com.example.angiday.profile
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.angiday.R
-import com.example.angiday.profile.ProfileActivity
+import com.example.angiday.activity.LoginActivity
+import com.example.angiday.activity.MainActivity
 
-class MainActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_profile)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val loginBtn = findViewById<TextView>(R.id.loginText)
 
-        // Lấy TextView (thay vì Button)
-        val profileText = findViewById<TextView>(R.id.ProfileText)
-
-        // Khi bấm vào TextView thì chuyển sang ProfileActivity
-        profileText.setOnClickListener {
-            startActivity(Intent(this, ProfileActivity::class.java))
+        // Khi bấm thì chuyển sang MainActivity
+        loginBtn.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
