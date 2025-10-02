7e63fe8bfae11bdccad13d17b4a05eefd4114fab
Nam <naml75803@gmail.com>
2025-10-02 13:58:15 +0700

    themprofile

---
+++ app/src/main/java/com/example/angiday/ui/profile/Edit_pfActivity.kt
@@ -0,0 +1,14 @@
+package com.example.angiday.ui.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.angiday.R
+
+class Edit_pfActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_edit_pf)
+
+    }
+}
