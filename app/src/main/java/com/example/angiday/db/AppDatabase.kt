c7036ca4cc10a6d1d5fe5d11acf55a074e1b5024
Fate <trantanphuc1105@gmail.com>
2025-11-17 08:50:10 +0700

    fixdb

--- app/src/main/java/com/example/angiday/db/AppDatabase.kt
+++ app/src/main/java/com/example/angiday/db/AppDatabase.kt
@@ -8,6 +8,7 @@ import androidx.room.TypeConverters
 import com.example.angiday.db.dao.UserBehaviorDao
 import com.example.angiday.db.dao.UserDao
 import com.example.angiday.db.dao.UserProfileDao
+import com.example.angiday.db.providers.DatabaseCopier
 import com.example.angiday.model.entity.*
 
 @Database(
@@ -38,15 +39,20 @@ abstract class AppDatabase : RoomDatabase() {
 
         fun get(context: Context): AppDatabase =
             INSTANCE ?: synchronized(this) {
+
+                // COPY DB nếu chưa tồn tại
+                DatabaseCopier.copyPrepopulatedDB(context)
+
                 INSTANCE ?: Room.databaseBuilder(
                     context.applicationContext,
-                    AppDatabase::class.java, "angiday.db"
+                    AppDatabase::class.java,
+                    "angiday.db"
                 )
-                    .createFromAsset("databases/angiday.db")
                     .fallbackToDestructiveMigration()
-                    .allowMainThreadQueries() // chỉ nên bật khi test
+                    .allowMainThreadQueries()
                     .build()
                     .also { INSTANCE = it }
             }
+
     }
 }
